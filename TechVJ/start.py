87a0bd0555f5ee70eaf6aba6f64388232df3cb5c
usrhtpawar09-star <usrhtpawar09@gmail.com>
2026-02-04 22:06:39 +0530

    Update start.py

--- TechVJ/start.py
+++ TechVJ/start.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit Tg - @VJ_Bots
+# Don't Remove Credit Tg - @usrhtff01
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Ask Doubt on telegram @usrhtff009
 
 import os
 import asyncio 
